89703cc789bc1c4ca83844128f458d4076490245
Temberland <120428640+Temberland@users.noreply.github.com>
2023-05-09 19:44:10 +1000

    Класс для реализации стобцов

---
+++ DrawColumn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TaskBoard
+{
+    public class DrawColumn
+    {
+        public Border Ret()
+        {
+            int a = 480; // Ширина
+            int b = 640; // Высота досок
+            Border btn = new Border();
+            btn.Background = Brushes.Blue;
+            btn.BorderBrush = Brushes.Black;
+            btn.CornerRadius = new CornerRadius(25);
+            btn.Width = a;
+            btn.Height = b;
+            btn.BorderThickness = new Thickness(4); // толщина границы: 2 пикселя сверху, 4 пикселя справа, 6 пикселей снизу, 8 пикселей слева
+            btn.Margin = new Thickness(20, 100, 0, 0); // расположение элемента в контейнере задается с помощью свойства Margin и объекта Thickness
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Top;
+            return btn;
+        }
+    }
+}
